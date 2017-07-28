c05438117de9942b2737c107f847b620eb1a2c82
Thykof <nathan.seva@outlook.fr>
2017-07-28 19:20:53 +0200

    Simplify base.py

--- imeditor/filters/base.py
+++ imeditor/filters/base.py
@@ -13,36 +13,32 @@ def negative(img):
         green = 255 - pixel[1]
         blue = 255 - pixel[2]
         data_m.append((red, green, blue))
-    img_m = Image.new(img.mode, img.size)
-    img_m.putdata(data_m)
-    return img_m
+    img.putdata(data_m)
+    return img
 
 def red(img):
     data = list(img.getdata())
     data_m = list()
     for pixel in data:
         data_m.append((pixel[0], 0, 0))
-    img_m = Image.new(img.mode, img.size)
-    img_m.putdata(data_m)
-    return img_m
+    img.putdata(data_m)
+    return img
 
 def green(img):
     data = list(img.getdata())
     data_m = list()
     for pixel in data:
         data_m.append((0, pixel[1], 0))
-    img_m = Image.new(img.mode, img.size)
-    img_m.putdata(data_m)
-    return img_m
+    img.putdata(data_m)
+    return img
 
 def blue(img):
     data = list(img.getdata())
     data_m = list()
     for pixel in data:
         data_m.append((0, 0, pixel[2]))
-    img_m = Image.new(img.mode, img.size)
-    img_m.putdata(data_m)
-    return img_m
+    img.putdata(data_m)
+    return img
 
 def grayscale(img):
     data = list(img.getdata())
@@ -50,9 +46,8 @@ def grayscale(img):
     for pixel in data:
         gray = (pixel[0] + pixel[1] + pixel[2]) // 3
         data_m.append((gray, gray, gray))
-    img_m = Image.new(img.mode, img.size)
-    img_m.putdata(data_m)
-    return img_m
+    img.putdata(data_m)
+    return img
 
 def black_white(img, limit):
     data = list(img.getdata())
@@ -61,27 +56,24 @@ def black_white(img, limit):
         gray = (pixel[0] + pixel[1] + pixel[2]) // 3
         gray = 0 if gray < limit else 255
         data_m.append((gray, gray, gray))
-    img_m = Image.new(img.mode, img.size)
-    img_m.putdata(data_m)
-    return img_m
+    img.putdata(data_m)
+    return img
 
 def lighten(img, value):
     data = list(img.getdata())
     data_m = list()
     for pixel in data:
         data_m.append((pixel[0]+value, pixel[1]+value, pixel[2]+value))
-    img_m = Image.new(img.mode, img.size)
-    img_m.putdata(data_m)
-    return img_m
+    img.putdata(data_m)
+    return img
 
 def darken(img, value):
     data = list(img.getdata())
     data_m = list()
     for pixel in data:
         data_m.append((pixel[0]-value, pixel[1]-value, pixel[2]-value))
-    img_m = Image.new(img.mode, img.size)
-    img_m.putdata(data_m)
-    return img_m
+    img.putdata(data_m)
+    return img
 
 def rotate_left(img):
     return img.rotate(90, expand=True)
