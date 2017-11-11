7fdc0de0b1860065bb5a1de56ba9e7f91a1b94d0
Hugo Posnic <hugo.posnic@gmail.com>
2017-11-11 23:09:01 +0100

    Don't return an unused var

--- imeditor/editor/draw.py
+++ imeditor/editor/draw.py
@@ -9,14 +9,14 @@ def draw_shape(img, coords, size):
         size /= 2
     x = [coords[0][0] - size, coords[0][1] - size]
     y = [coords[1][0] + size, coords[1][1] + size]
-    return draw, size, x + y
+    return draw, x + y
 
 def draw_rectangle(img, coords, size, fill_color=None, outline_color=None):
     """Draw a rectangle on an image"""
-    draw, size, xy = draw_shape(img, coords, size)
+    draw, xy = draw_shape(img, coords, size)
     draw.rectangle(xy, fill=fill_color, outline=outline_color)
 
 def draw_ellipse(img, coords, size, fill_color=None, outline_color=None):
     """Draw an ellipse on an image"""
-    draw, size, xy = draw_shape(img, coords, size)
+    draw, xy = draw_shape(img, coords, size)
     draw.ellipse(xy, fill=fill_color, outline=outline_color)
